e9ad8f540a9c468dbe2416e6de3bbdeb6f04838a
Ankit-kumar2764 <ay9335144776@gmail.com>
2026-04-07 12:25:54 +0530

    project20

---
+++ project20/src/App.jsx
@@ -0,0 +1,9 @@
+function app() {
+  return (
+    <div style ={{ color:"white", backgroundColor:"blue", padding:"10px",margin:"300px"}}>
+      <h2>hello react</h2>
+      <p>this is inline css</p>
+    </div>
+  );
+}
+export default app;
