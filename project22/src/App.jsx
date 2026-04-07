1e426057a12fdfb21d2c6e71427a533d4ce40ecf
Ankit-kumar2764 <ay9335144776@gmail.com>
2026-04-07 12:57:17 +0530

    project22

---
+++ project22/src/App.jsx
@@ -0,0 +1,9 @@
+import style from "./App.module.css";
+function App() {
+  return (
+    <div className={style.container}>
+      <h2>Module css example</h2>
+    </div>
+  );
+}
+export default App;
